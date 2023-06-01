5d47d718456a8fcdbf73b43df45cf42079895bf8
mattiatoffanin <mattia.toffanin.1@studenti.unipd.it>
2023-06-01 17:56:14 +0200

    Fix hw3 comments

--- G023HW3.py
+++ G023HW3.py
@@ -6,39 +6,39 @@ import sys
 import random as rand
 import numpy as np
 
-# Number of items to read
-THRESHOLD = 1000
+THRESHOLD = 10000000  # Number of items to read
 
 p = 8191  # Prime number
-a = rand.randint(1, p - 1)  # Random number at each invocation
-b = rand.randint(0, p - 1)  # Random number at each invocation
+a = rand.randint(1, p - 1)  # Random number at each run
+b = rand.randint(0, p - 1)  # Random number at each run
 
 
+# Hash function
 def h(u, j):
     return (((a * u + b) % p) * j) % W
 
 
+# Hash function
 def g(u, j):
     return -1 if (u * j) % 2 == 0 else +1
 
 
 def process_batch(time, batch):
-    # We are working on the batch at time `time`.
-    global streamLength, histogram, C, left, right, D, W
+    global streamLength, true_frequencies, C, left, right, D, W
     batch_size = batch.count()
-    streamLength[0] += batch_size
-    # Extract the distinct items from the batch
-    batch_items = batch.map(lambda s: int(s)).filter(lambda x: x >= left and x <= right).collect()
-    streamLength[1] += len(batch_items)
+    streamLength[0] += batch_size  # Increment the entire stream length
+    # Extract the distinct items from the batch in [left; right]
+    batch_items = batch.map(lambda s: int(s)).filter(lambda x: left <= x <= right).collect()
+    streamLength[1] += len(batch_items)  # Increment the number of items processed
 
-    for item in batch_items:
+    for xt in batch_items:
         for j in range(D):
-            C[j, h(item, j)] += g(item, j)
+            C[j, h(xt, j)] += g(xt, j)
 
-        if item not in histogram:
-            histogram[item] = 1
+        if xt not in true_frequencies:
+            true_frequencies[xt] = 1
         else:
-            histogram[item] += 1
+            true_frequencies[xt] += 1
 
     if batch_size > 0:
         print("Batch size at time [{0}] is: {1}".format(time, batch_size))
@@ -100,9 +100,9 @@ if __name__ == '__main__':
     print("Port number =", portExp)
 
     # Data structures to maintain the state of the stream
-    streamLength = [0, 0]
-    histogram = {}
-    C = np.zeros((D, W))
+    streamLength = [0, 0]  # i0: entire stream length, i1: filtered stream length
+    true_frequencies = {}  # dictionary to store the true frequencies
+    C = np.zeros((D, W))  # matrix to calculate the approximate frequencies
 
     # Stream creation
     stream = ssc.socketTextStream("algo.dei.unipd.it", portExp, StorageLevel.MEMORY_AND_DISK)
@@ -118,37 +118,42 @@ if __name__ == '__main__':
     ssc.stop(False, True)
     print("Streaming engine stopped")
 
-    fu = {}
+    # Approximate frequency calculation
+    appr_frequencies = {}  # dictionary to store the approximate frequencies
     for item in range(left, right + 1):
-        fu[item] = np.median([g(item, j) * C[j, h(item, j)] for j in range(D)])
+        appr_frequencies[item] = int(np.median([g(item, j) * C[j, h(item, j)] for j in range(D)]))
 
-    f2j = np.zeros(D)
+    # Approximate second moment calculation
+    f2j = np.zeros(D)  # list to store intermediate approximate second moments
     for j in range(D):
         for k in range(W):
             f2j[j] += C[j, k] ** 2
-    f2 = np.median(f2j)
-    f2 /= streamLength[1] ** 2
+    appr_second_moment = np.median(f2j)
+    appr_second_moment /= streamLength[1] ** 2
 
-    true_F2 = 0
-    for k in histogram:
-        true_F2 += histogram[k] ** 2
-    true_F2 /= streamLength[1] ** 2
+    # True second moment calculation
+    true_second_moment = 0
+    for k in true_frequencies:
+        true_second_moment += true_frequencies[k] ** 2
+    true_second_moment /= streamLength[1] ** 2
 
-    histogram = dict(sorted(histogram.items(), key=lambda item: item[1]))
+    # Order by value (frequencies) true_frequencies dictionary
+    true_frequencies = dict(sorted(true_frequencies.items(), key=lambda it: it[1]))
 
-    histogram_list = list(histogram.items())[-K:]
+    # Create a list with the K most frequent elements
+    true_frequencies_list = list(true_frequencies.items())[-K:]
 
+    # Average relative error calculation
     cumulative = 0
-    for (item, freq) in histogram_list:
-        cumulative += abs(freq - fu[item]) / freq
-    mean = cumulative / K
-
-    print(mean)
-
-    print(histogram_list)
+    for (item, freq) in true_frequencies_list:
+        cumulative += abs(freq - appr_frequencies[item]) / freq
+    average_relative_error = cumulative / K
 
     # Print output
     print("Number of items processed =", streamLength[0])
-    print("Number of distinct items =", len(histogram))
-    largest_item = max(histogram.keys())
-    print("Largest item =", largest_item)
+    print("Number of filtered items processed =", streamLength[1])
+    print("Number of distinct filtered items =", len(appr_frequencies))
+    print("Average relative error =", average_relative_error)
+    if K <= 20:
+        for (item, freq) in true_frequencies_list:
+            print("Item", item, "True frequency", freq, "Approximate frequency", appr_frequencies[item])
